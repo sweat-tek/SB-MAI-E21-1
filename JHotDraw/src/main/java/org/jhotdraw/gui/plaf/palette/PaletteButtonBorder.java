a5d06b6d38fa93e71b1745575516e7ddf2ac6f3d
Mlinnelyst <mlinnelyst11@gmail.com>
2021-11-28 15:41:45 +0100

    Fix #15 Created an abstract class which implements the duplicated functionality in one place.

--- JHotDraw/src/main/java/org/jhotdraw/gui/plaf/palette/PaletteButtonBorder.java
+++ JHotDraw/src/main/java/org/jhotdraw/gui/plaf/palette/PaletteButtonBorder.java
@@ -16,26 +16,26 @@ package org.jhotdraw.gui.plaf.palette;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Insets;
-import java.awt.geom.Point2D;
 import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.plaf.UIResource;
-import org.apache.batik.ext.awt.*;
+import org.jhotdraw.geom.Dimensions2D;
+import org.jhotdraw.geom.Position2D;
 
 /**
  * PaletteButtonBorder.
  *
  * @author Werner Randelshofer
- * @version 1.0 Apr 6, 2008 Created.
+ *          @version 1.0 Apr 6, 2008 Created.
  */
-public class PaletteButtonBorder implements Border, UIResource {
+public class PaletteButtonBorder extends BorderPaint implements Border, UIResource {
 
-    private final static float[] enabledStops = new float[]{0f, 0.35f, 0.4f, 1f};
-    private final static Color[] enabledStopColors = new Color[]{new Color(0xf8f8f8), new Color(0xeeeeee), new Color(0xcacaca), new Color(0xffffff)};
-    private final static float[] selectedStops = new float[]{0f, 0.1f, 0.9f, 1f};
-    private final static Color[] selectedStopColors = new Color[]{new Color(0x666666), new Color(0xcccccc), new Color(0x999999), new Color(0xb1b1b1)};
+    private final static float[] enabledStops = new float[] { 0f, 0.35f, 0.4f, 1f };
+    private final static Color[] enabledStopColors = new Color[] { new Color(0xf8f8f8), new Color(0xeeeeee),
+            new Color(0xcacaca), new Color(0xffffff) };
+    private final static float[] selectedStops = new float[] { 0f, 0.1f, 0.9f, 1f };
+    private final static Color[] selectedStopColors = new Color[] { new Color(0x666666), new Color(0xcccccc),
+            new Color(0x999999), new Color(0xb1b1b1) };
 
     public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
         if (c instanceof AbstractButton) {
@@ -44,64 +44,22 @@ public class PaletteButtonBorder implements Border, UIResource {
     }
 
     public void paintBorder(AbstractButton c, Graphics gr, int x, int y, int width, int height) {
-        Graphics2D g = (Graphics2D) gr;
         ButtonModel m = c.getModel();
-        int borderColor;
-        float[] stops;
-        Color[] stopColors;
+        int borderColor = 0xffa5a5a5;
+        float[] stops = enabledStops;
+        Color[] stopColors = enabledStopColors;
+
         if (!m.isEnabled()) {
             borderColor = 0x80a5a5a5;
             stops = enabledStops;
             stopColors = enabledStopColors;
-        } else {
-            if (m.isSelected() || m.isPressed() && m.isArmed()) {
-                borderColor = 0xff333333;
-                stops = selectedStops;
-                stopColors = selectedStopColors;
-            } else {
-                borderColor = 0xffa5a5a5;
-                stops = enabledStops;
-                stopColors = enabledStopColors;
-            }
+        } else if (m.isSelected() || m.isPressed() && m.isArmed()) {
+            borderColor = 0xff333333;
+            stops = selectedStops;
+            stopColors = selectedStopColors;
         }
-        String segmentPosition = getSegmentPosition(c);
-        if (segmentPosition == "first" || segmentPosition == "middle") {
-            width += 1;
-        }
-        g.setColor(new Color(borderColor, true));
-        g.drawRect(x, y, width - 1, height - 1);
 
-        LinearGradientPaint lgp = new LinearGradientPaint(
-                new Point2D.Float(x, y), new Point2D.Float(x, y + height - 1),
-                stops, stopColors,
-                MultipleGradientPaint.REPEAT,
-                MultipleGradientPaint.LINEAR_RGB);
-        g.setPaint(lgp);
-        g.fillRect(x + 1, y + 1, width - 2, height - 2);
-    }
-
-    private String getSegmentPosition(Component c) {
-        String segmentPosition = null;
-        if (c instanceof JComponent) {
-        segmentPosition = (String) ((JComponent) c).getClientProperty("Palette.Component.segmentPosition");
-        }
-        return (segmentPosition == null) ? "only" : segmentPosition;
-    }
-
-    public Insets getBorderInsets(Component c) {
-        Insets insets;
-        String segmentPosition = getSegmentPosition(c);
-        if (segmentPosition == "first" ||
-                segmentPosition == "middle") {
-            insets = new Insets(3, 3, 3, 2);
-        } else {
-            insets = new Insets(3, 3, 3, 3);
-        }
-        return insets;
-    }
-
-
-    public boolean isBorderOpaque() {
-        return true;
+        paintBorderImplementation(c, gr, new Position2D(x, y), new Dimensions2D(width, height), enabledStops,
+                enabledStopColors, borderColor, stops, stopColors);
     }
 }
--- JHotDraw/src/main/java/org/jhotdraw/gui/plaf/palette/PaletteTextComponentBorder.java
+++ JHotDraw/src/main/java/org/jhotdraw/gui/plaf/palette/PaletteTextComponentBorder.java
@@ -16,78 +16,36 @@ package org.jhotdraw.gui.plaf.palette;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Insets;
-import java.awt.geom.Point2D;
-import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.plaf.UIResource;
-import org.apache.batik.ext.awt.*;
+import org.jhotdraw.geom.Dimensions2D;
+import org.jhotdraw.geom.Position2D;
 
 /**
  * PaletteTextComponentBorder.
  *
  * @author Werner Randelshofer
- * @version 1.0 Apr 6, 2008 Created.
+ *          @version 1.0 Apr 6, 2008 Created.
  */
-public class PaletteTextComponentBorder implements Border, UIResource {
+public class PaletteTextComponentBorder extends BorderPaint implements Border, UIResource {
 
-    private final static float[] enabledStops = new float[]{0f, 0.2f};
-    private final static Color[] enabledStopColors = new Color[]{new Color(0xc8c8c8), new Color(0xffffff)};
+    private final static float[] enabledStops = new float[] { 0f, 0.2f };
+    private final static Color[] enabledStopColors = new Color[] { new Color(0xc8c8c8), new Color(0xffffff) };
 
     public void paintBorder(Component c, Graphics gr, int x, int y, int width, int height) {
-        Graphics2D g = (Graphics2D) gr;
-        JComponent jc = (JComponent) c;
-        int borderColor;
-        float[] stops;
-        Color[] stopColors;
+        int borderColor = 0xffa5a5a5;
+        float[] stops = enabledStops;
+        Color[] stopColors = enabledStopColors;
+
         if (!c.isEnabled()) {
             borderColor = 0x80a5a5a5;
             stops = enabledStops;
             stopColors = enabledStopColors;
-        } else {
-            borderColor = 0xffa5a5a5;
-            stops = enabledStops;
-            stopColors = enabledStopColors;
         }
 
-        String segmentPosition = getSegmentPosition(c);
-        if (segmentPosition == "first" || segmentPosition == "middle") {
-            width += 1;
-        }
-        g.setColor(new Color(borderColor, true));
-        g.drawRect(x, y, width - 1, height - 1);
+        Position2D pos = new Position2D(x, y);
+        Dimensions2D dim = new Dimensions2D(width, height);
 
-        LinearGradientPaint lgp = new LinearGradientPaint(
-                new Point2D.Float(x, y), new Point2D.Float(x, y + height - 1),
-                stops, stopColors,
-                MultipleGradientPaint.REPEAT,
-                MultipleGradientPaint.LINEAR_RGB);
-        g.setPaint(lgp);
-        g.fillRect(x + 1, y + 1, width - 2, height - 2);
-    }
-
-    private String getSegmentPosition(Component c) {
-        String segmentPosition = null;
-        if (c instanceof JComponent) {
-        segmentPosition = (String) ((JComponent) c).getClientProperty("Palette.Component.segmentPosition");
-        }
-        return (segmentPosition == null) ? "only" : segmentPosition;
-    }
-
-    public Insets getBorderInsets(Component c) {
-        Insets insets;
-        String segmentPosition = getSegmentPosition(c);
-        if (segmentPosition == "first" ||
-                segmentPosition == "middle") {
-            insets = new Insets(3, 3, 3, 2);
-        } else {
-            insets = new Insets(3, 3, 3, 3);
-        }
-        return insets;
-    }
-
-    public boolean isBorderOpaque() {
-        return true;
+        paintBorderImplementation(c, gr, pos, dim, enabledStops, enabledStopColors, borderColor, stops, stopColors);
     }
 }
